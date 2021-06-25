dc76bda225a390aea7f40f33472987755a6c3713
samlior <samlior@foxmail.com>
2021-06-25 10:49:08 +0800

    add Boost::log

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,6 +1,10 @@
-﻿#include "json.h"
+﻿#include <boost/log/trivial.hpp>
+#include <boost/date_time.hpp>
+#include "json.h"
 #include "cinatra.hpp"
+
 using namespace cinatra;
+using namespace boost::gregorian;
 
 inline std::string make_json_string(const Json::Value &value)
 {
@@ -10,6 +14,17 @@ inline std::string make_json_string(const Json::Value &value)
 
 int main()
 {
+	std::string s("2001-10-9"); //2001-October-09
+	date d(from_simple_string(s));
+	BOOST_LOG_TRIVIAL(info) << to_simple_string(d);
+
+	BOOST_LOG_TRIVIAL(trace) << "A trace severity message";
+	BOOST_LOG_TRIVIAL(debug) << "A debug severity message";
+	BOOST_LOG_TRIVIAL(info) << "An informational severity message";
+	BOOST_LOG_TRIVIAL(warning) << "A warning severity message";
+	BOOST_LOG_TRIVIAL(error) << "An error severity message";
+	BOOST_LOG_TRIVIAL(fatal) << "A fatal severity message";
+
 	int max_thread_num = std::thread::hardware_concurrency();
 	http_server server(max_thread_num);
 	server.listen("0.0.0.0", "8080");
